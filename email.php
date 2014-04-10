50b2d9fe8e8639974ce5f32042dd2dd29f25b4e8
leckster <leckie.gunter@byu.edu>
2014-04-10 00:26:19 -0600

    got email working :) :)

--- email.php
+++ email.php
@@ -5,8 +5,20 @@ use Mailgun\Mailgun;
 $class_id = $_POST['class_id'];
 echo ($class_id);
 
-$roll = file_get_contents("cs462.local/index.php/signin/class_id/1");
+$roll = json_decode(file_get_contents("http://cs462.local/signin/class_id/1"));
+echo "got roll";
+// Instantiate the client.
+$mg = new Mailgun('key-3l2x11ofeiqvyy1zs88073cxbtsmqxp2');
+echo "got mailer";
+var_dump($mg);
+$domain = "sandbox03919bfa91e54f2f964446440ef8b3e9.mailgun.org";
 
-var_dump($roll);
+echo "here";
+
+// Now, compose and send your message.
+$mg->sendMessage($domain, array('from'    => 'Mailgun Sandbox <postmaster@sandbox03919bfa91e54f2f964446440ef8b3e9.mailgun.org>',
+                                'to'      => 'Leckie <leckster@gmail.com>', 
+                                'subject' => 'Class Roll', 
+                                'text'    => $roll));
 
 echo "success";
